0afed25aa866f6da27f22df140c6e188c726a593
VitorCampos28 <vito.r.natal@hotmail.com>
2020-04-10 16:45:04 -0300

    Convertendo matriz em Posicao xadrez

--- Program.cs
+++ Program.cs
@@ -8,19 +8,11 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                GameTable table = new GameTable(8, 8);
-                table.placePiece(new Tower(table, Color.Black), new Position(0, 0));
-                table.placePiece(new Tower(table, Color.Black), new Position(0, 0));
-                table.placePiece(new King(table, Color.Black), new Position(2, 4));
-                PlaceChessTable.placechess(table);
-            }
+            ChessPosition pos = new ChessPosition('c', 7);
 
-            catch(TableExceptions e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toChessPosition());
 
             Console.ReadLine();
         }
